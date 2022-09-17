3de8badad1ff436cdff74b40e1d4d1cd808603ab
skyv26 <dev.aakashv@gmail.com>
2022-09-17 23:15:04 +0530

    Configure Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
+import AddBook from './components/AddBook/AddBook';
+import Categories from './components/Categories/Categories';
 import './App.css';
 
 const App = () => (
   <>
     <Header />
+    <Routes>
+      <Route path="/" element={<AddBook />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/books" render={() => <Navigate to="/" />} />
+    </Routes>
+    {/* <AddBook /> */}
   </>
 );
 
