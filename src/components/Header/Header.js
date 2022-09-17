6bedc474aea9d5bdd13b46cec11f64218d7a6edf
skyv26 <dev.aakashv@gmail.com>
2022-09-17 22:43:04 +0530

    Update `Header` component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import './Header.module.css';
+import style from './Header.module.css';
 
 const Header = () => (
-  <></>
+  <>
+    <header className={style.Header}>
+      <nav className={style.Navigation}>
+        <ul className={style.UnorderedList}>
+          <li className={style.List}>
+            Books
+          </li>
+          <li className={style.List}>
+            Categories
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </>
 );
 
 export default Header;
