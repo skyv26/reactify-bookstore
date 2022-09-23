221932d8653858a4e600ca9be0b44d9c06d8a514
skyv26 <dev.aakashv@gmail.com>
2022-09-23 23:48:48 +0530

    dispatch `category` action

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Categories = () => (
-  <button type="button">Check Status</button>
-);
+const Categories = () => {
+  const [status, updateStatus] = useState(false);
+  const [category] = useSelector((state) => state.categoriesReducer);
+  const dispatch = useDispatch();
+  const checkStatusHandler = () => {
+    updateStatus((prevState) => !prevState);
+    dispatch({ type: '' });
+  };
+
+  return (
+    <>
+      <button type="button" onClick={checkStatusHandler}>Check Status</button>
+      { status ? <p>{category.text}</p> : null }
+    </>
+  );
+};
 
 export default Categories;
