c70c4b4986a5eda1e90f63207bff26091b608ed4
skyv26 <dev.aakashv@gmail.com>
2022-09-23 19:24:25 +0530

    Add `action-creators`

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,17 @@
+// Action Type
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
+// Default Data
 const initialState = [];
+
+// Action Creators
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (book) => ({
+  type: REMOVE_BOOK,
+  book,
+});
