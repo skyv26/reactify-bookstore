fb118222afb91b42387a6190d21215177064d220
skyv26 <dev.aakashv@gmail.com>
2022-09-24 18:39:55 +0530

    Add `API link`

--- src/redux/reduxThunk/reduxThunk.js
+++ src/redux/reduxThunk/reduxThunk.js
@@ -2,3 +2,6 @@
 const ADD_BOOK = 'books/api/newBook';
 const REMOVE_BOOK = 'books/api/removeBook';
 const FETCH_BOOK = 'books/api/fetchBook';
+
+// API
+const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ehEXJIZOdMZTzHuhOZWq/books';
