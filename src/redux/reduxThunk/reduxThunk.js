9ee4afaa68068f32a59f13047ea4430fc51a62ea
skyv26 <dev.aakashv@gmail.com>
2022-09-24 18:43:03 +0530

    Add `createAsyncThunk` for addBook

--- src/redux/reduxThunk/reduxThunk.js
+++ src/redux/reduxThunk/reduxThunk.js
@@ -1,3 +1,6 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { addBook } from "../books/books";
+
 // ACTION TYPES
 const ADD_BOOK = 'books/api/newBook';
 const REMOVE_BOOK = 'books/api/removeBook';
@@ -5,3 +8,15 @@ const FETCH_BOOK = 'books/api/fetchBook';
 
 // API
 const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ehEXJIZOdMZTzHuhOZWq/books';
+
+export const addBookThunk = createAsyncThunk(ADD_BOOK, async (action) => {
+  const { payload, dispatch } = action;
+  await fetch(API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })();
+  dispatch(addBook(payload));
+});
