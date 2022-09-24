9c8f655ce4493e2900bc899a4e14a02178fc5fc5
skyv26 <dev.aakashv@gmail.com>
2022-09-24 18:52:04 +0530

    Update `redux/[books.js]` file

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,24 +5,28 @@ const REMOVE_BOOK = 'REMOVE_BOOK';
 // Default Data
 const initialState = [
   {
-    id: 1,
+    item_id: 1,
     title: 'Game of Life',
     author: 'Mr. Secret',
+    category: 'Suspense',
   },
   {
-    id: 2,
+    item_id: 2,
     title: 'Awesome Guy',
     author: 'Miss Bethary',
+    category: 'Biography',
   },
   {
-    id: 3,
+    item_id: 3,
     title: 'Coding Jokes',
     author: 'Aakash Verma',
+    category: 'Drama',
   },
   {
     id: 4,
     title: 'Uncertain Goal',
     author: 'Secretu',
+    category: 'Comedy',
   },
 ];
 
@@ -45,7 +49,7 @@ const booksReducer = (state = initialState, action) => {
       action.payload,
     ];
     case REMOVE_BOOK: return [
-      ...state.filter((each) => each.id !== action.payload.id),
+      ...state.filter((each) => each.item_id !== action.payload.item_id),
     ];
     default: return state;
   }
